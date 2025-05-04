be0ca5a6f04b793085e3d2d2c5a6a85899a4e0cc
Adu-3 <S202157950@kfupm.edu.sa>
2025-05-04 08:41:20 +0300

    Guest Header

--- src/components/Guest/GuestHeader.jsx
+++ src/components/Guest/GuestHeader.jsx
@@ -7,17 +7,14 @@ const GuestHeader = () => {
     <header className="guest-header">
       <nav className="guest-nav">
         <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>
-          Home
+        <div className="flex items-center space-x-2">
+        <span className="material-icons bg-[linear-gradient(60deg,#0095FF,#00FFCC)] bg-clip-text text-transparent">directions_car</span>
+        <h1 className="font-bold bg-[linear-gradient(60deg,#0095FF,#00FFCC)] bg-clip-text text-transparent text-xl">Car Service</h1>
+
+        </div>
         </NavLink>
       </nav>
 
-      <div className="guest-icons">
-        <NavLink to="/login">
-          <span className="material-icons profile-icon" style={{ cursor: 'pointer' }}>
-            person
-          </span>
-        </NavLink>
-      </div>
     </header>
   );
 };
