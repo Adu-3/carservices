73f16b2c88b8838364b80aee3bddc83ec23ebaaf
Adu-3 <S202157950@kfupm.edu.sa>
2025-05-05 03:41:38 +0300

    Login + Register Logic HTTPS

--- src/components/Guest/Register.js
+++ src/components/Guest/Register.js
@@ -21,7 +21,7 @@ function Register() {
     }
 
     try {
-      const response = await fetch('http://localhost:5000/api/register', {
+      const response = await fetch('https://project.dubaisi.net//api/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password, email }),
